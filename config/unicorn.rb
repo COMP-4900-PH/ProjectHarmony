1e136fcfaf75724122fc968c310af8f19c36e5b6
Jox <joxched@gmail.com>
2016-04-30 20:52:16 -0700

    unicorn setup

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/deploy/ProjectHarmony"
+pid "/home/deploy/ProjectHarmony/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/ProjectHarmony/unicorn.log"
+stdout_path "/home/deploy/ProjectHarmony/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
