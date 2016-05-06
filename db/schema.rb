ecb116a3b3bad42627e551eafc86e4c3206ed2cd
Mike Lewis <mike_lewis_@hotmail.com>
2016-05-06 13:53:06 -0700

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -57,56 +57,6 @@ ActiveRecord::Schema.define(version: 20160505175628) do
 
   add_index "events", ["sailing_id"], name: "index_events_on_sailing_id"
 
-  create_table "forem_categories", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "forem_forums", force: :cascade do |t|
-    t.string "title"
-    t.text   "description"
-  end
-
-  create_table "forem_posts", force: :cascade do |t|
-    t.integer  "topic_id"
-    t.text     "text"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reply_to_id"
-  end
-
-  add_index "forem_posts", ["reply_to_id"], name: "index_forem_posts_on_reply_to_id"
-  add_index "forem_posts", ["topic_id"], name: "index_forem_posts_on_topic_id"
-  add_index "forem_posts", ["user_id"], name: "index_forem_posts_on_user_id"
-
-  create_table "forem_topics", force: :cascade do |t|
-    t.integer  "forum_id"
-    t.integer  "user_id"
-    t.string   "subject"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "locked",     default: false, null: false
-    t.boolean  "pinned",     default: false
-    t.boolean  "hidden",     default: false
-  end
-
-  add_index "forem_topics", ["forum_id"], name: "index_forem_topics_on_forum_id"
-  add_index "forem_topics", ["user_id"], name: "index_forem_topics_on_user_id"
-
-  create_table "forem_views", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "count",      default: 0
-  end
-
-  add_index "forem_views", ["topic_id"], name: "index_forem_views_on_topic_id"
-  add_index "forem_views", ["updated_at"], name: "index_forem_views_on_updated_at"
-  add_index "forem_views", ["user_id"], name: "index_forem_views_on_user_id"
-
   create_table "non_registered_users", force: :cascade do |t|
     t.integer  "age"
     t.string   "gender"
