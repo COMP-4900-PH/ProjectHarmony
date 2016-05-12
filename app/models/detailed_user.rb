663ff5780ca2415f6103e2f9d0914be48ddb361d
Jox <joxched@gmail.com>
2016-05-12 16:04:59 -0700

    add image to event

--- app/models/detailed_user.rb
+++ app/models/detailed_user.rb
@@ -1,6 +1,6 @@
 class DetailedUser < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar, styles: { thumb: "240x270#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { small:"100x100", thumb: "240x270#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512054151) do
+ActiveRecord::Schema.define(version: 20160512215557) do
 
   create_table "detailed_users", force: :cascade do |t|
     t.string   "first_name"
@@ -55,8 +55,12 @@ ActiveRecord::Schema.define(version: 20160512054151) do
     t.text     "description"
     t.integer  "max_participants"
     t.integer  "sailing_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "events", ["sailing_id"], name: "index_events_on_sailing_id"
