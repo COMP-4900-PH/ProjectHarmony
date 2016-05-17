8f6e015121d1c0dfbd8e3ccca7cd99e018bb624b
Jox <joxched@gmail.com>
2016-05-17 01:49:00 -0700

    comment on popover method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,11 @@ class UsersController < ApplicationController
     @detailed_user = DetailedUser.find_by_user_id(params[:id])
   end
 
+  # use by the hover function in events page
+  # where it queries the information of the user asynchronously
+  # when you hover on its image
+  # returns json
   def popover
-
     sql = "select event_name, cruise_ship_name from event_registers JOIN events ON event_registers.event_id = events.id JOIN sailings ON events.sailing_id = sailings.id  WHERE user_id = #{params[:id]}"
     records_array = ActiveRecord::Base.connection.execute(sql)
    render json: records_array
