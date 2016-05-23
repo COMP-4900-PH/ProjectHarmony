bc97169a0c4046fe315a5c9f91d99c23cc59875c
Mike Lewis <mike_lewis_@hotmail.com>
2016-05-22 19:45:53 -0700

    added dynamic data to home page, added and changed a few image files, added some logic in detailed_user and sailing models

--- app/models/detailed_user.rb
+++ app/models/detailed_user.rb
@@ -3,5 +3,9 @@ class DetailedUser < ActiveRecord::Base
   has_attached_file :avatar, styles: { small:"100x100", thumb: "240x270#" }, default_url: "/images/user/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+  def user_age
+    (Date.today.to_time.to_i - self.birth_day.to_i) / 60 / 60 / 24 / 365
+  end
+
 end
 
--- app/models/sailing.rb
+++ app/models/sailing.rb
@@ -2,8 +2,36 @@ class Sailing < ActiveRecord::Base
   has_many :travelling_parties
   has_many :events
 
+  attr_accessor :people_on_sailing, :array_of_things, :number_of_english_speakers
+
   # Calculate the amount of days until the ship leaves
   def daysToSailing
     (self.departure_date.to_time.to_i - Date.today.to_time.to_i) / 60 / 60 / 24
   end
+
+  def primary_language
+    gather_data.map do |users|
+      users.primary_language
+    end
+  end
+
+  def secondary_language
+    gather_data.map do |users|
+      users.secondary_language
+    end
+  end
+
+  def people_on_sailing
+    @people_on_sailing = gather_data.count
+  end
+
+  def gather_data
+
+    self.travelling_parties.flat_map do |party|
+      party.party_registers.map do |reg|
+        reg.user.detailed_user
+      end
+    end
+  end
+
 end
